95ce09e768db1d1ee772521c5ec4883961b6041c
Kenan Nalbant <kanalbant@gmail.com>
2015-07-02 11:54:28 -0700

    WOW! even more stuff

--- htmlDownload.hs
+++ htmlDownload.hs
@@ -1,12 +1,15 @@
-import Network.HTTP
+import Network.HTTP 
 import Data.List 
-import Data.List.Split
+import Data.List.Split 
 import System.Directory 
 import Data.Char 
+import Text.Read
+import Data.Maybe
+import Control.Arrow
 
-
-courseURLs = "http://www.cs.ucdavis.edu/courses/descriptions/"
-
+courseURLs           = "http://www.cs.ucdavis.edu/courses/descriptions/"
+graduateCourseNumber = 200
+courseDirectory      = "exp_course_desc"
 
 extractLinks :: String -> [String]
 extractLinks = cleanup . filterRefs . splitOnTags  
@@ -15,34 +18,51 @@ extractLinks = cleanup . filterRefs . splitOnTags
         cleanup         = map ( (!! 1)  . splitWhen (orEqual '\"' '\'' ))
         orEqual o1 o2 s = s == o1 || s == o2
 
-
 filterECSLinks :: [String] -> [String] 
 filterECSLinks = filter ("ecs" `isInfixOf`)  
 
-getCourseHTML :: IO String
-getCourseHTML = do 
-  response <- simpleHTTP (getRequest courseURLs)
+getHTMLBody:: String -> IO String
+getHTMLBody url = do 
+  response <- simpleHTTP (getRequest url)
   getResponseBody response
 
 getECSLinks :: String -> [String] 
 getECSLinks = filterECSLinks . extractLinks 
 
-
-numberStringToInt :: String -> Int 
-numberStringToInt = read . cleanNumber
-
+numberStringToInt :: String -> Maybe Int 
+numberStringToInt = readMaybe . cleanNumber
   where cleanNumber []     = []
         cleanNumber (x:xs) = if isDigit x then x : cleanNumber xs else cleanNumber xs
 
+getCourseInt :: String -> Int 
+getCourseInt = fromJustList . filter isJust . maybeIntList  
+  where maybeIntList = map numberStringToInt . splitOn "-" 
+        fromJustList [x] = fromJust x
 
+isUndergraduateCourse = (<graduateCourseNumber) . getCourseInt
+
+capitalize :: String -> String 
+capitalize = map toUpper 
+
+getCourseTitle s =  capitalize $ addTheL $ (!! 1) $ splitOn "-" s
+  where addTheL st = if "89A" `isSuffixOf` capitalize st then st ++ "-L" else st --a hacky fix but a fix  
+        
+zipURLsAndTitles = map (getCourseTitle &&& id) 
+
+getTitlesAndUrls :: String -> [String] 
+getTitlesAndUrls = zipURLsAndTitles . filter isUndergraduateCourse . getECSLinks 
 
 main = do 
- 
-  rawHTML <- getCourseHTML
+  rawHTML <- getHTMLBody courseURLs 
+  let titlesAndURLs = getTitlesAndUrls rawHTML
 
-  let ecsLinks = getECSLinks rawHTML
+  createDirectoryIfMissing False "exp_course_dec" 
 
-  mapM_ print ecsLinks
 
+  print titlesAndURLs
 
   return rawHTML
+
+
+
+
